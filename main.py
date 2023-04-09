42eab3cb6cc3eb94f30f370ae1fb35c5cbb9fca5
PH-KDX <smtp.python.email.sender@gmail.com>
2023-04-09 22:56:04 +0200

    edit comment newlines

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ log = logging.getLogger("discord")
 TEST_GUILD = discord.Object(id=582974704864854016)
 TEST_GUILD_TWO = discord.Object(id=1088169718038417501)
 
-@bot.tree.command(name = "commandname", description = "My first application Command", guild=TEST_GUILD) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+@bot.tree.command(name = "commandname", description = "My first application Command", guild=TEST_GUILD)
+# Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
 async def first_command(interaction):
     await interaction.response.send_message("Hello!")
 
@@ -30,7 +31,7 @@ async def setup_hook():
 @bot.event
 async def on_guild_available(ctx):
     bot.tree.copy_global_to(guild=ctx)
-    # since on_ready can be called in a restart, the tree sync is run on the first guild join
+    # for every guild, as it comes ready, the tree is synced for that guild
     log.info(f"starting command tree sync for guild {ctx.id}")
     await bot.tree.sync(guild=ctx)
     log.info(f"command tree sync completed for guild {ctx.id}")
