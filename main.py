dc1a630582a136322e5a9cd28f444a710d5972ef
PH-KDX <smtp.python.email.sender@gmail.com>
2023-04-11 16:12:25 +0200

    remove placeholder command

--- main.py
+++ main.py
@@ -15,11 +15,6 @@ log = logging.getLogger("discord")
 TEST_GUILD = discord.Object(id=582974704864854016)
 TEST_GUILD_TWO = discord.Object(id=1088169718038417501)
 
-@bot.tree.command(name = "commandname", description = "My first application Command", guild=TEST_GUILD)
-# Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
-async def first_command(interaction):
-    await interaction.response.send_message("Hello!")
-
 extensions = ["utils", "planning"]
 
 @bot.event
