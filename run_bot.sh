bc3ef010e7b5be4006a73a8df128b31a78c32ee5
PH-KDX <smtp.python.email.sender@gmail.com>
2023-04-11 18:12:24 +0200

    correctly set exit codes on script failure

--- run_bot.sh
+++ run_bot.sh
@@ -14,14 +14,14 @@ if [ ! -d "venv" ]; then
         echo "Finished setup, deactivating and continuing normally"
     else
         echo "Python 3.10 not found in system, please install it to continue."
-        exit
+        exit 1
     fi
 fi
 echo "Activating virtual environment"
 source venv/bin/activate
 if { python3.10 -c 'import sys; print(".".join(map(str, sys.version_info[:3])))'; echo "3.10"; } | sort --version-sort --check &> /dev/null; then
   echo "Virtual environment python version is less than 3.10. Please delete the venv directory and run this script again to create a new one."
-  exit
+  exit 1
 fi
 cd src
 echo "Starting bot"
