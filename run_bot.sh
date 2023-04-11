698226e22ac78486b73039bd4ced05c3193939fc
PH-KDX <smtp.python.email.sender@gmail.com>
2023-04-11 18:31:35 +0200

    fix bugs in shell script

--- run_bot.sh
+++ run_bot.sh
@@ -1,16 +1,15 @@
 if [ ! -d "venv" ]; then
     echo "No virtual environment found with name 'venv', checking system python version"
-    python_installed=false
-    # only set python_installed to true if precisely python3.10 is installed
-    command -v python3.10 >/dev/null 2>&1 && python_installed=true
-    if python_installed; then
+    if command -v python3.10 >/dev/null 2>&1; then
         echo "Confirmed Python 3.10 installed in system"
         echo "Generating vitual environment"
         python3.10 -m venv venv
         echo "Activating environment"
         source venv/bin/activate
+        echo "Updating pip"
+        pip install --upgrade pip
         echo "Installing requirements"
-        pip install -m requirements.txt
+        pip install -r requirements.txt
         echo "Finished setup, deactivating and continuing normally"
     else
         echo "Python 3.10 not found in system, please install it to continue."
@@ -19,7 +18,7 @@ if [ ! -d "venv" ]; then
 fi
 echo "Activating virtual environment"
 source venv/bin/activate
-if { python3.10 -c 'import sys; print(".".join(map(str, sys.version_info[:3])))'; echo "3.10"; } | sort --version-sort --check &> /dev/null; then
+if { python -c 'import sys; print(".".join(map(str, sys.version_info[:3])))'; echo "3.10"; } | sort --version-sort --check &> /dev/null; then
   echo "Virtual environment python version is less than 3.10. Please delete the venv directory and run this script again to create a new one."
   exit 1
 fi
